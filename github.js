2f99c3ca571de37a7aad41dc1e6a873d2c747623
Preston Tom-Ying <preston.tomying@gmail.com>
2023-05-04 18:39:44 -0400

    this code should work

--- github.js
+++ github.js
@@ -7,8 +7,31 @@ const { Octokit } = require("@octokit/core");
 const csv = require("csv-parser");
 const fs = require("fs");
 
-const results = [];
+const invite_not_sent = -1;
+const invite_pending = 0;
+const invite_accepted = 1;
 
+// CSV WRITTER
+// write to csv file
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: "Test2.csv",
+  header: [
+    { id: "first_Name", title: "first_Name" },
+    { id: "last_Name", title: "last_Name" },
+    { id: "email", title: "email" },
+    { id: "github", title: "github" },
+    { id: "discord_username", title: "discord_username" },
+    { id: "team", title: "team" },
+    { id: "project", title: "project" },
+    { id: "joined_Github", title: "joined_Github" },
+    { id: "joined_Discord", title: "joined_Discord" },
+    { id: "joined_Clickup", title: "joined_Clickup" },
+  ],
+  append: true, // set to true to append to an existing file
+});
+const data = {};
+const results = [];
 // Read the CSV file
 fs.createReadStream("Test2.csv")
   .pipe(csv())
@@ -28,23 +51,32 @@ fs.createReadStream("Test2.csv")
         discord_username: row.discord_username,
         team: row.team,
         project: row.project,
+        joined_Github: row.joined_Github,
         joined_Discord: row.joined_Discord,
         joined_Clickup: row.joined_Clickup,
-        joined_Github: row.joined_Github,
       };
 
       console.log(row.email, row.team, row.github);
-      sendInvitation(row.email, row.team, row.github);
-      console.log("Invite sent for " + row.first_Name + "\n");
-
-      // catch errors here
-      data[8].row.joined_Github = 0; // send invite
-
-      csvWriter
-        .writeRecords(data)
-        .then(() => console.log("The CSV file was updated successfully"));
+      if (data[row.id].joined_Github == invite_not_sent) {
+        sendInvitation(row.email, row.team, row.github);
+        data[row.id].joined_Github = 0; // change invite status
+        console.log(data[row.id].joined_Github);
+        console.log("Invite sent for " + row.name + "\n");
+      } else if (
+        data[row.id].joined_Github == invite_pending ||
+        data[row.id].joined_Github == invite_accepted
+      ) {
+        console.log("Already sent invite");
+      }
     }
   });
+
+// Update csv with status
+csvWriter
+  .writeRecords(data)
+  .then(() => console.log("The CSV file was updated successfully"));
+
+console.log(data);
 
 async function sendInvitation(receiver_email, team_name, github_user) {
   const octokit = new Octokit({
@@ -74,27 +106,3 @@ async function sendInvitation(receiver_email, team_name, github_user) {
     }
   );
 }
-
-// write to csv file
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-  path: "Test2.csv",
-  header: [
-    { id: "first_Name", title: "first_Name" },
-    { id: "last_Name", title: "last_Name" },
-    { id: "email", title: "email" },
-    { id: "github", title: "github" },
-    { id: "discord_username", title: "discord_username" },
-    { id: "team", title: "team" },
-    { id: "project", title: "project" },
-    { id: "joined_Github", title: "joined_Github" },
-    { id: "joined_Discord", title: "joined_Discord" },
-    { id: "joined_Clickup", title: "joined_Clickup" },
-  ],
-  append: true, // set to true to append to an existing file
-});
-
-// const data = [
-//   {name: 'Alice', age: '28', gender: 'Female'},
-//   {name: 'Bob', age: '35', gender: 'Male'},
-// ];
